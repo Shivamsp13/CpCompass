a9dd7314085e0c9f1968e46a6ca60985267d90c8
Shivamsp13 <shivamofficialsp13@gmail.com>
2026-07-09 12:34:16 +0530

    Upadted CustomUserDetailsService

--- cp-compass-backend/src/main/java/com/cpcompass/service/CustomUserDetailsService.java
+++ cp-compass-backend/src/main/java/com/cpcompass/service/CustomUserDetailsService.java
@@ -3,26 +3,30 @@ package com.cpcompass.service;
 import com.cpcompass.entity.User;
 import com.cpcompass.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public UserDetails loadUserByUsername(String email) {
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
 
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException(
-                                "User not found"
-                        ));
+        com.cpcompass.entity.User user =
+                userRepository.findByEmail(email)
+                        .orElseThrow(() ->
+                                new UsernameNotFoundException(
+                                        "User not found"
+                                ));
 
-        return org.springframework.security.core.userdetails.User
-                .builder()
+        return User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
                 .authorities("USER")
